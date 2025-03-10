7279fe0a4527ec947f7055715be1cf598e2e427f
Aryan Rathi <aryan2001rathi@gmail.com>
2025-03-10 21:16:12 +0530

    update the mongoDB string and made some other changesdd

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 //connecting database
 
- mongoose.connect("mongodb+srv://wolfstarc24:LYP1CQzjMbC7siIJ@notesapi.wvmom.mongodb.net/")
-//mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log("Server started at port number " + PORT);
